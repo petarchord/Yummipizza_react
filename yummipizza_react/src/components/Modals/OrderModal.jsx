53443ca62013a142dcd9eaf4d133d10d5f2b2249
petarchord <petarchord@hotmail.com>
2020-08-01 20:20:08 +0200

    added modal , modal issue solved

--- yummipizza_react/src/components/Modals/OrderModal.jsx
+++ yummipizza_react/src/components/Modals/OrderModal.jsx
@@ -13,29 +13,55 @@ const OrderModal = ({ open, setModal }) => {
         onRequestClose={() => {
           setModal(false);
         }}
+        // className={styles.portal}
+        // overlayClassName={styles.overlay_modal}
+        style={{
+          overlay: {
+            position: "fixed",
+            height: "80%",
+            width: "50%",
+            top: "5%",
+            left: "25%",
+            backgroundColor: "rgba(255,255,255,1,0)",
+            textAlign: "center",
+          },
+          content: {
+            position: "absolute",
+            textAlign: "center",
+            padding: "10px",
+            margin: "20px",
+          },
+        }}
       >
         <h3>Enter your information</h3>
         <button
           onClick={() => {
             setModal(false);
           }}
+          id={styles.cancel_button}
         >
           x
         </button>
         <form action="">
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input type="text" name="name" />
+          <div className={styles.input_group}>
+            <div className={styles.name_field}>
+              <label htmlFor="name">Name: </label>
+              <input type="text" name="name" />
+            </div>
+            <div className={styles.address_field}>
+              <label htmlFor="address">Address: </label>
+              <input type="text" name="address" />
+            </div>
+            <div className={styles.phone_field}>
+              <label htmlFor="phone">Phone: </label>
+              <input type="number" name="phone" />
+            </div>
+            <input
+              type="submit"
+              value="Confirm your order"
+              id={styles.submit}
+            />
           </div>
-          <div>
-            <label htmlFor="address">Address</label>
-            <input type="text" name="address" />
-          </div>
-          <div>
-            <label htmlFor="phone">Address</label>
-            <input type="number" name="phone" />
-          </div>
-          <input type="submit" value="Confirm your order" />
         </form>
       </Modal>
     </div>
